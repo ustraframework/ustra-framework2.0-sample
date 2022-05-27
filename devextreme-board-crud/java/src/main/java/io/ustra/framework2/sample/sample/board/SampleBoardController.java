1da1b1dee69bc71b4068ceb603709d2613bf668d
Roy Lee <keymasroy@gmail.com>
2022-05-27 14:48:44 +0900

    Devextreme 게시판 Java 샘플 업데이트

---
+++ devextreme-board-crud/java/src/main/java/io/ustra/framework2/sample/sample/board/SampleBoardController.java
@@ -0,0 +1,35 @@
+package io.ustra.framework2.sample.sample.board;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/sample/board")
+public class SampleBoardController {
+
+	@Autowired private SampleBoardService sampleBoardService;
+
+	@GetMapping("")
+	List<SampleBoardModel> getList(String title) {
+		return sampleBoardService.getList(title);
+	}
+
+	@GetMapping("/{boardIdx}")
+	SampleBoardModel getCodeGroups(@PathVariable Integer boardIdx) {
+		return sampleBoardService.get(boardIdx);
+	}
+
+	@PostMapping("")
+	SampleBoardModel add(@RequestBody SampleBoardModel board) {
+		return sampleBoardService.add(board);
+	}
+
+
+}
