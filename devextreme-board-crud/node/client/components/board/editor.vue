fc349eaac71fc1b46c1d2b80f792d7799c68ac4f
Roy Lee <keymasroy@gmail.com>
2022-07-04 12:43:48 +0900

    스마트 에디터 초기 모듈화 처리

--- devextreme-board-crud/node/client/components/board/editor.vue
+++ devextreme-board-crud/node/client/components/board/editor.vue
@@ -1,95 +1,81 @@
 <template>
-  <DxHtmlEditor :value="value" height="725px">
-    <DxMediaResizing :enabled="true" />
-    <DxToolbar :multiline="true">
-      <DxItem name="undo" />
-      <DxItem name="redo" />
-      <DxItem name="separator" />
-      <DxItem :accepted-values="sizeValues" name="size" />
-      <DxItem :accepted-values="fontValues" name="font" />
-      <DxItem name="separator" />
-      <DxItem name="bold" />
-      <DxItem name="italic" />
-      <DxItem name="strike" />
-      <DxItem name="underline" />
-      <DxItem name="separator" />
-      <DxItem name="alignLeft" />
-      <DxItem name="alignCenter" />
-      <DxItem name="alignRight" />
-      <DxItem name="alignJustify" />
-      <DxItem name="separator" />
-      <DxItem name="orderedList" />
-      <DxItem name="bulletList" />
-      <DxItem name="separator" />
-      <DxItem :accepted-values="headerValues" name="header" />
-      <DxItem name="separator" />
-      <DxItem name="color" />
-      <DxItem name="background" />
-      <DxItem name="separator" />
-      <DxItem name="link" />
-      <DxItem name="image" />
-      <DxItem name="separator" />
-      <DxItem name="clear" />
-      <DxItem name="codeBlock" />
-      <DxItem name="blockquote" />
-      <DxItem name="separator" />
-      <DxItem name="insertTable" />
-      <DxItem name="deleteTable" />
-      <DxItem name="insertRowAbove" />
-      <DxItem name="insertRowBelow" />
-      <DxItem name="deleteRow" />
-      <DxItem name="insertColumnLeft" />
-      <DxItem name="insertColumnRight" />
-      <DxItem name="deleteColumn" />
-    </DxToolbar>
-  </DxHtmlEditor>
+  <textarea id="editor1" ref="editorText"></textarea>
 </template>
 <script lang="ts">
-import { Component } from 'vue-property-decorator'
+import { Component, Ref, Model, Watch } from 'vue-property-decorator'
 import { UstraBoComponent } from '@ustra/nuxt-mng-bo/src/components/ustra-bo-component'
-import { DxHtmlEditor, DxToolbar, DxMediaResizing, DxItem } from 'devextreme-vue/html-editor'
+import { clearInterval, setInterval } from 'timers'
+
+declare var nhn: any
 
 @Component({
-  components: { DxHtmlEditor, DxToolbar, DxMediaResizing, DxItem },
+  components: {},
+  head: {
+    script: [{ type: 'text/javascript', src: '/smarteditor2-2.10.0/js/service/HuskyEZCreator.js', charset: 'utf-8' }],
+  },
 })
 export default class extends UstraBoComponent {
-  // #region variables
-  value: string = null
-  sizeValues = ['8pt', '10pt', '12pt', '14pt', '18pt', '24pt', '36pt']
-  fontValues = ['Arial', 'Courier New', 'Georgia', 'Impact', 'Lucida Console', 'Tahoma', 'Times New Roman', 'Verdana']
-  headerValues = [false, 1, 2, 3, 4, 5]
+  @Ref() readonly editorText: HTMLTextAreaElement
+  @Model('value', { default: null }) readonly value
 
-  // #endregion
-  // #region hooks
-  // #endregion
-  // #region methods
-  // #endregion
-  // #region watches
-  // #endregion
+  editorLoaded: boolean = false
+  rawEditorObject: any = null
+  rawEditorObjects: any[] = []
+  valueSyncTimer: any = null
+
+  mounted() {
+    this.createEditor()
+  }
+
+  async getRawEditorObject() {
+    return await this.$ustra.utils.core.getObjectAsync(() =>
+      this.rawEditorObjects && this.rawEditorObjects.length > 0 && this.rawEditorObjects[0].setRawHTMLContents ? this.rawEditorObjects[0] : null,
+    )
+  }
+
+  async createEditor() {
+    if (this.editorLoaded) {
+      return
+    }
+
+    this.editorLoaded = true
+
+    const nhn = await this.$ustra.utils.core.getObjectAsync(() => window['nhn'])
+
+    nhn.husky.EZCreator.createInIFrame({
+      oAppRef: this.rawEditorObjects,
+      elPlaceHolder: 'editor1',
+      sSkinURI: '/smarteditor2-2.10.0/SmartEditor2Skin.html',
+      fCreator: 'createSEditor2',
+    })
+
+    this.startValueSyncTimer()
+  }
+
+  async startValueSyncTimer() {
+    if (this.valueSyncTimer) {
+      clearInterval(this.valueSyncTimer)
+    }
+
+    const editor = await this.getRawEditorObject()
+    setInterval(() => {
+      this.$emit('value', editor.getRawHTMLContents())
+    }, 1500)
+  }
+
+  beforeDestroy() {
+    if (this.valueSyncTimer) {
+      clearInterval(this.valueSyncTimer)
+    }
+  }
+
+  @Watch('value', { immediate: true })
+  async valueChanged(v) {
+    const editor = await this.getRawEditorObject()
+
+    if (v !== editor.getRawHTMLContents()) {
+      editor.setRawHTMLContents(!v ? '' : v)
+    }
+  }
 }
 </script>
-<style lang="scss">
-.dx-htmleditor-content img {
-  vertical-align: middle;
-  padding-right: 10px;
-}
-
-.dx-htmleditor-content table {
-  width: 50%;
-}
-
-.options {
-  padding: 20px;
-  background-color: rgba(191, 191, 191, 0.15);
-  margin-top: 20px;
-}
-
-.caption {
-  font-size: 18px;
-  font-weight: 500;
-}
-
-.option {
-  margin-top: 10px;
-}
-</style>
