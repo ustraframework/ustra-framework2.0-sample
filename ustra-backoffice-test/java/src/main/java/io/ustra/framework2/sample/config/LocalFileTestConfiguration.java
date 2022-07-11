a3f78d99c9b96db5246fe6f167d98406aee0dd68
jhjcoding <77096242+jhjcoding@users.noreply.github.com>
2022-07-11 11:32:55 +0900

    주석 추가

--- ustra-backoffice-test/java/src/main/java/io/ustra/framework2/sample/config/LocalFileTestConfiguration.java
+++ ustra-backoffice-test/java/src/main/java/io/ustra/framework2/sample/config/LocalFileTestConfiguration.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 
+import com.gsitm.ustra.java.data.file.config.FileOperationConfigurer;
 import com.gsitm.ustra.java.data.file.policy.FileGroupPolicyStore;
 import com.gsitm.ustra.java.data.file.processor.AbstractFileOperationProcessor;
+import com.gsitm.ustra.java.data.file.processor.storage.FileResourceStorage;
+import com.gsitm.ustra.java.data.file.processor.storage.LocalFileResourceStorage;
 import com.gsitm.ustra.java.management.data.file.UstraManagementFileOperationProcessor;
 
 @Configuration
@@ -15,7 +18,17 @@ public class LocalFileTestConfiguration {
 	@Bean
 	@Order(Ordered.LOWEST_PRECEDENCE - 2)
 	public AbstractFileOperationProcessor managementLocalFileTestOperationProcessor(FileGroupPolicyStore fileGroupPolicyStore) {
-		return new UstraManagementFileOperationProcessor(fileGroupPolicyStore) {
+
+//		FileOperationConfigurer config = new FileOperationConfigurer() {
+//
+//			@Override
+//			public FileResourceStorage fileResourceStorage() {
+//				return new XXXFileResourceStorage();
+//			}
+//
+//		};
+
+		return new UstraManagementFileOperationProcessor(fileGroupPolicyStore /*, config */) {
 			@Override
 			public boolean supports(String fileGrpId) {
 				return "test-local-upload".contains(fileGrpId);
