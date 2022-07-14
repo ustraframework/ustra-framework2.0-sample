ce2a87ce7136b8a89bd5a8675b20b8d61c25f1a7
정철우 <wisehouse@gsitm.com>
2022-07-14 17:55:15 +0900

    배치 작업 코드 수정

--- batch/task/src/main/java/com/gsitm/ustra/tutorial/batch/task/SimpleTask.java
+++ batch/task/src/main/java/com/gsitm/ustra/tutorial/batch/task/SimpleTask.java
@@ -1,23 +1,12 @@
 package com.gsitm.ustra.tutorial.batch.task;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.sql.DataSource;
-
+import com.gsitm.ustra.java.batch.task.UstraBatchSimpleTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import com.gsitm.ustra.java.batch.task.UstraBatchSimpleTask;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Component("SIMPLE_TASK")
 public class SimpleTask extends UstraBatchSimpleTask {
@@ -25,6 +14,8 @@ public class SimpleTask extends UstraBatchSimpleTask {
 
     @Override
     protected void execute() {
-        LOGGER.info("{} | SIMPLE TASK runs", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        final String datetimeString = LocalDateTime.now().format(formatter);
+        LOGGER.info("{} | SIMPLE TASK runs", datetimeString);
     }
 }
