f063c198f1041f9cb885c6942788c09ca5dd8eda
정철우 <wisehouse@gsitm.com>
2022-07-22 18:45:47 +0900

    그레이드 대응

--- batch/console-runner/src/main/java/com/gsitm/ustra/tutorial/batch/consolerunner/ConsoleRunner.java
+++ batch/console-runner/src/main/java/com/gsitm/ustra/tutorial/batch/consolerunner/ConsoleRunner.java
@@ -1,15 +1,12 @@
 package com.gsitm.ustra.tutorial.batch.consolerunner;
 
 import com.gsitm.ustra.java.batch.runner.CommandLineTaskRunner;
+import com.gsitm.ustra.java.core.app.ConsoleApplicationRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConsoleRunner extends CommandLineTaskRunner {
-    public ConsoleRunner() {
-        super(ConsoleRunner.class);
-    }
-
-    public static void main(String ...args) {
-        ConsoleRunner.run(ConsoleRunner.class, args);
+    public static void main(String ...args) throws Exception {
+        ConsoleApplicationRunner.run(ConsoleRunner.class, args);
     }
 }
